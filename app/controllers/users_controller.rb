e60eae3dc91e23b5a93d13ff7bd2ed72d294fadc
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-09-07 23:34:38 +0900

    testuser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
-def reset_password
-  user = User.find_by(email: "test@user")
-  if user
-    user.password = "testuser"
-    user.password_confirmation = "testuser"
-    if user.save
-      render plain: "✅ パスワードをリセットしました"
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:reset_password]
+
+  def reset_password
+    user = User.find_by(email: "tarotaro@com")
+    if user
+      user.password = "newpassword"
+      user.password_confirmation = "newpassword"
+      if user.save(validate: false)
+        render plain: "✅ パスワードをリセットしました"
+      else
+        render plain: "❌ 保存失敗: #{user.errors.full_messages.join(', ')}"
+      end
     else
-      render plain: "❌ 保存失敗: #{user.errors.full_messages.join(', ')}"
+      render plain: "❌ ユーザーが見つかりません"
     end
-  else
-    render plain: "❌ ユーザーが見つかりません"
   end
 end
